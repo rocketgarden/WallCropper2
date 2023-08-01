6f0683bb926255b1f846a69d14269e1bf9948a6d
Vince Blas <blas.vince@gmail.com>
2023-08-01 02:31:30 -0700

    Remove file loading from CropPreviewImage
    Massive improvements to compose and coroutine/flow judo. Thanks ChatGPT

--- src/jvmMain/kotlin/net/vinceblas/wallcropper2/Main.kt
+++ src/jvmMain/kotlin/net/vinceblas/wallcropper2/Main.kt
@@ -9,33 +9,62 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.WindowState
 import androidx.compose.ui.window.singleWindowApplication
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import net.vinceblas.wallcropper2.components.CropPreviewImage
+import net.vinceblas.wallcropper2.components.CropState
+import java.io.File
 
+const val widePic = "picdir/test169wide.png"
+const val bigPic = "picdir/weirdeagle.jpg"
 
 @Composable
 @Preview
-fun App() {
-    @Suppress("UNUSED_VARIABLE") val widePic = "picdir/test169wide.png"
-    @Suppress("UNUSED_VARIABLE") val bigpic = "picdir/weirdeagle.jpg"
+fun App(imageFilePathFlow: MutableStateFlow<String>) {
+    val composableScope = rememberCoroutineScope()
 
-    var text by remember { mutableStateOf("Hello, World!") }
+    val imageByteArrayFlow: StateFlow<ByteArray?> = imageFilePathFlow
+        .filter { it.isNotEmpty() }
+        .map { filePath ->
+            withContext(Dispatchers.IO) {
+                File(filePath).readBytes()
+            }
+        }.stateIn(composableScope, SharingStarted.Eagerly, null)
+    // stateIn converts this to a stateFlow so we can remember the last value and access it later when we want to crop/save the image
+
+    val cropStateFlow = remember { MutableStateFlow<CropState?>(null) }
+    // not actually sure if remember is necessary here since we'd be feeding a new initial state in on recompose anyways? maybe?
+
+    // feed loaded images into PreviewImage state
+    composableScope.launch {
+        imageByteArrayFlow.collect { bytes ->
+            bytes?.let { cropStateFlow.value = CropState.buildInitialState(bytes) }
+        }
+    }
 
     MaterialTheme {
         Column {
-            CropPreviewImage(bigpic)
+            CropPreviewImage(cropStateFlow)
+
             Button(onClick = {
-                text = "Hello, Desktop!"
+                // simulate loading next image
+                imageFilePathFlow.value = bigPic
             }) {
-                Text(text)
+                Text("Next image")
             }
         }
     }
 }
 
+
 fun main() {
+    // declare master flow outside compose scope so it isn't recreated on recompose
+    val imageFilePathFlow = MutableStateFlow(widePic)
     singleWindowApplication(
         state = WindowState(width = 1500.dp, height = 1300.dp)
     ) {
-        App()
+        App(imageFilePathFlow)
     }
 }
--- src/jvmMain/kotlin/net/vinceblas/wallcropper2/components/CropPreviewImage.kt
+++ src/jvmMain/kotlin/net/vinceblas/wallcropper2/components/CropPreviewImage.kt
@@ -5,7 +5,8 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.gestures.onDrag
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.DrawModifier
 import androidx.compose.ui.geometry.Offset
@@ -14,71 +15,99 @@ import androidx.compose.ui.geometry.isUnspecified
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.drawscope.ContentDrawScope
-import androidx.compose.ui.graphics.painter.BitmapPainter
-import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.graphics.toComposeImageBitmap
 import androidx.compose.ui.layout.onSizeChanged
-import androidx.compose.ui.res.loadImageBitmap
 import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.round
 import androidx.compose.ui.unit.toOffset
 import androidx.compose.ui.unit.toSize
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
-import java.io.File
-import java.io.IOException
+import kotlinx.coroutines.flow.MutableStateFlow
+import org.jetbrains.skia.Image
 
 data class CropState(
     // all dimensions are relative to the image, not the preview on screen
-    val ratio: Double = 16.0 / 9.0,
+    val imageBitmap: ImageBitmap,
+    val desiredRatio: Double,
     val imageSize: IntSize = IntSize.Zero,
     val cropImageOffset: Offset = Offset.Zero,
     val scaleFactor: Float = Float.NaN
     // multiply by scaleFactor to go from image -> screen dimensions
     // divide for reverse
-)
+
+
+) {
+    companion object {
+        // mostly just encapsulating crop offset calculation
+        fun buildInitialState(byteArray: ByteArray, ratio: Double = 16.0 / 9.0): CropState {
+            val bitmap = Image.makeFromEncoded(byteArray).toComposeImageBitmap()
+            val imageSize = IntSize(bitmap.width, bitmap.height)
+            val desiredSize = getDesiredSizeForRatio(imageSize, ratio)
+            val cropOffset =
+                Offset((imageSize.width - desiredSize.width) / 2f, (imageSize.height - desiredSize.height) / 2f)
+
+            return CropState(bitmap, ratio, imageSize, cropOffset)
+        }
+    }
+
+}
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
-fun CropPreviewImage(filepath: String) { // todo have this take a stream for state publishing instead
+fun CropPreviewImage(stateFlow: MutableStateFlow<CropState?>) {
+    val state = stateFlow.collectAsState().value
 
-    var state by remember { mutableStateOf(CropState()) }
-
-    Column {
-        AsyncImage(
-            load = { loadImageBitmap(File(filepath)) },
-            afterLoad = { bitmap -> state = onImageLoaded(state, bitmap) },
-            painterFor = { remember { BitmapPainter(it) } },
-            modifier = Modifier.onSizeChanged { renderSize ->
-                if (renderSize.width > 0 && renderSize.height > 0) {
-                    state = state.copy(scaleFactor = (renderSize.height.toFloat() / state.imageSize.height.toFloat()))
+    state?.let { cropState ->
+        Column {
+            Image(
+                bitmap = cropState.imageBitmap,
+                contentDescription = null,
+                modifier = Modifier.onSizeChanged { renderSize ->
+                    // capture scaleFactor after render or resize
+                    if (renderSize.width > 0 && renderSize.height > 0) {
+                        stateFlow.value =
+                            cropState.copy(scaleFactor = (renderSize.height.toFloat() / cropState.imageSize.height.toFloat()))
+                    }
                 }
-            }
-                .then( // awkward conditional modifiers
-                    if (state.scaleFactor.isFinite())
-                    // don't draw overlay until image is loaded and measured
-                        CropPreviewOverlay(state).onDrag { dragOffset ->
-                            state = state.copy(cropImageOffset = getNewOffset(state, dragOffset))
-                        }
-                    else Modifier
-                )
-        )
-        Text("Raw Imagesize ${state.imageSize}")
-        Text("Desired size ${getDesiredSizeForRatio(state.imageSize, state.ratio)}")
+                    .then( // awkward conditional modifiers
+                        if (cropState.scaleFactor.isFinite())
+                        // don't draw overlay until image is measured and we know scaleFactor
+                            CropPreviewOverlayModifier(cropState)
+                                .onDrag { dragOffset ->
+                                    stateFlow.value =
+                                        cropState.copy(cropImageOffset = getNewOffset(cropState, dragOffset))
+                                }
+                        else Modifier
+                    )
+            )
 
-        Text("Crop Offset ${state.cropImageOffset}")
-        Text("Scalefactor ${state.scaleFactor}")
+            // todo remove debug info or hide behind a flag
+            Text("Raw Imagesize ${cropState.imageSize}")
+            Text("Desired size ${getDesiredSizeForRatio(cropState.imageSize, cropState.desiredRatio)}")
 
-        Text("Desired scaled size ${getDesiredSizeForRatio(state.imageSize, state.ratio) * state.scaleFactor}")
-        Text("Scaled Imagesize ${state.imageSize.toSize() * state.scaleFactor}")
+            Text("Crop Offset ${cropState.cropImageOffset}")
+            Text("Scalefactor ${cropState.scaleFactor}")
+
+            Text(
+                "Desired scaled size ${
+                    getDesiredSizeForRatio(
+                        cropState.imageSize,
+                        cropState.desiredRatio
+                    ) * cropState.scaleFactor
+                }"
+            )
+            Text("Scaled Imagesize ${cropState.imageSize.toSize() * cropState.scaleFactor}")
+        }
     }
 }
 
+/**
+ * Convert screenSpace drag offset to imageSpace offset for crop window, bounded by image edges
+ */
 private fun getNewOffset(state: CropState, dragOffset: Offset): Offset {
     val size = state.imageSize
     if (size == IntSize.Zero) return state.cropImageOffset
 
-    val desiredSize = getDesiredSizeForRatio(state.imageSize, state.ratio)
+    val desiredSize = getDesiredSizeForRatio(state.imageSize, state.desiredRatio)
 
     val xMax = (size.width - desiredSize.width)// maximum play in x direction
     val yMax = (size.height - desiredSize.height)
@@ -92,24 +121,14 @@ private fun getNewOffset(state: CropState, dragOffset: Offset): Offset {
     // rounding on each drag event means preview pane is always rendered in a "snapped" state
 }
 
-private fun onImageLoaded(state: CropState, bitmap: ImageBitmap): CropState {
-    val imageSize = IntSize(bitmap.width, bitmap.height)
-    val desiredSize = getDesiredSizeForRatio(imageSize, state.ratio)
-
-    //init crop window to center of image
-    val cropOffset = Offset((imageSize.width - desiredSize.width) / 2f, (imageSize.height - desiredSize.height) / 2f)
-
-    return state.copy(imageSize = imageSize, cropImageOffset = cropOffset)
-}
-
-class CropPreviewOverlay(private val state: CropState) : DrawModifier {
+class CropPreviewOverlayModifier(private val state: CropState) : DrawModifier {
     override fun ContentDrawScope.draw() {
         drawContent()
 
         if (state.cropImageOffset == Offset.Unspecified) return
         if (size.isUnspecified || size == Size.Zero) return
 
-        val cropRectSize = (getDesiredSizeForRatio(state.imageSize, ratio = state.ratio)) * state.scaleFactor
+        val cropRectSize = (getDesiredSizeForRatio(state.imageSize, ratio = state.desiredRatio)) * state.scaleFactor
         val previewOffset = state.cropImageOffset * state.scaleFactor
 //
         val alpha = 0.8f
@@ -149,46 +168,11 @@ class CropPreviewOverlay(private val state: CropState) : DrawModifier {
     }
 }
 
-@Composable
-fun <ImageBitmap> AsyncImage(
-    load: suspend () -> ImageBitmap,
-    afterLoad: (ImageBitmap) -> Unit = {},
-    painterFor: @Composable (ImageBitmap) -> Painter,
-    modifier: Modifier = Modifier,
-    contentScale: ContentScale = ContentScale.Fit,
-) {
-    val image: ImageBitmap? by produceState<ImageBitmap?>(null) {
-        value = withContext(Dispatchers.IO) {
-            try {
-                load()
-            } catch (e: IOException) {
-                // instead of printing to console, you can also write this to log,
-                // or show some error placeholder
-                e.printStackTrace()
-                null
-            }
-        }
-        value?.let(afterLoad)
-    }
-
-    if (image != null) {
-        Image(
-            painter = painterFor(image!!),
-            contentDescription = null,
-            contentScale = contentScale,
-            modifier = modifier
-        )
-    }
-}
-
-private fun loadImageBitmap(file: File): ImageBitmap =
-    file.inputStream().buffered().use(::loadImageBitmap)
-
 private fun getDesiredSizeForRatio(size: IntSize, ratio: Double): Size {
     if (size == IntSize.Zero) return Size.Zero
     return if (size.width / size.height > ratio) { // too wide, base size on height
         Size(width = (size.height * ratio).toFloat(), height = size.height.toFloat())
-    } else {
+    } else { // vice versa
         Size(width = size.width.toFloat(), height = (size.width / ratio).toFloat())
     }
 }
